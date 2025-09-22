f128877cdc608fd0444be9323d72ac88ea48af0d
CodeGen <161267485+Code-Gen-Hub@users.noreply.github.com>
2025-09-22 14:11:59 +0800

    Create PythonBasic.py

---
+++ PythonBasic.py
@@ -0,0 +1,12 @@
+# loop 循环
+# for loop
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+
+print() # insert blank line
+
+for greeting in range(5):
+  print('hello', greeting)
